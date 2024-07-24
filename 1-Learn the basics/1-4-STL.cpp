e3635b98cde99913c82fc89f7147172e8d9aa37d
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-07-24 13:35:45 +0530

    STL

--- 1-Learn the basics/1-4-STL.cpp
+++ 1-Learn the basics/1-4-STL.cpp
@@ -14,6 +14,21 @@ using namespace std;
 // 3. Derived containers :- Provides real world modelling. Eg: Stack, Queue, Priority queue
 
 
+// Pairs
+int main() {
+    pair<int, int> p = {1, 3};
+    cout << p.first << " " << p.second << endl;
+
+    pair<int, pair<int, int>> p2 = {1, {2, 3}};
+    cout << p2.first << " " << p2.second.first << " " << p2.second.second << endl;
+
+    pair<int, int> arr[] = {{1, 2}, {3, 4}, {5, 6}};
+    cout << arr[1].second << endl;
+
+    return 0;
+}
+
+
 // Vector
 // check the capacity, element access, modifiers, etc from "cplusplus.com/reference/"
 #include <vector>
