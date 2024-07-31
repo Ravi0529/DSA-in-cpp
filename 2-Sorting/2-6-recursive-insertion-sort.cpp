b7a0b1d08599c85bc43c785ca0d583e00737df97
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-07-31 09:31:15 +0530

    sorting

--- 2-Sorting/2-6-recursive-insertion-sort.cpp
+++ 2-Sorting/2-6-recursive-insertion-sort.cpp
@@ -3,22 +3,18 @@
 using namespace std;
 
 void insertion_sort_recursive(vector<int> &arr, int n) {
-    // Base case: If array size is 1 or smaller, return
     if (n <= 1) {
         return;
     }
 
     insertion_sort_recursive(arr, n - 1);
 
-    int last = arr[n - 1];
     int j = n - 2;
 
-    while (j >= 0 && arr[j] > last) {
-        arr[j + 1] = arr[j];
+    while (j >= 0 && arr[j] > arr[j + 1]) {
+        swap(arr[j], arr[j + 1]);
         j--;
     }
-
-    arr[j + 1] = last;
 }
 
 int main() {
