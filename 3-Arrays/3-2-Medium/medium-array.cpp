3ffdd01ea6307635931a2dfa14ab1869e8c6751e
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2024-08-27 12:29:26 +0530

    array

--- 3-Arrays/3-2-Medium/medium-array.cpp
+++ 3-Arrays/3-2-Medium/medium-array.cpp
@@ -370,6 +370,39 @@ void rotate(vector<vector<int>>& matrix) {
     }
 }
 
+// 12 ---> spiral order matrix
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int left = 0, right = m - 1;
+    int top = 0, bottom = n - 1;
+    vector<int> ans;
+
+    while(top <= bottom && left <= right) {
+        for(int i = left; i <= right; i++) {
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+        for(int i = top; i <= bottom; i++) {
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+        if(top <= bottom) {
+            for(int i = right; i >= left; i--) {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        if(left <= right) {
+            for(int i = bottom; i >= top; i--) {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans;
+}
+
 int main() {
     // 1 
     vector<int> arr = {1,2,3,4,5};
@@ -417,6 +450,10 @@ int main() {
     // 11
     vector<vector<int>> arr11 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     rotateImage(arr11);
+
+    // 12
+    vector<vector<int>> arr12 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    spiralOrder(arr12);
 
     return 0;
 }
